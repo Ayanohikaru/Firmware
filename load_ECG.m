fe3779bdd2cbfeb486f60f898de82728a0c1f5ad
unknown <ayanohikaru@gmail.com>
2018-06-23 08:59:22 +0700

    Seprarted ECC & Numeric

--- load_ECG.m
+++ load_ECG.m
@@ -1,2 +1,55 @@
-function m_ECG = load_ECG(m_name)
+function m_ECG = load_ECG(name)
+
+%% Setup
+Folder_edf = 'E:\Thesis\Firmware\Data\EDF';
+Folder_mat = 'E:\Thesis\Firmware\Data\Mat_ecg';
+Folder_header = 'E:\MIMIC II WAVEFORM DATABASE\RAW MIMIC II DATABASE\PRE_GENERATED RECORD';
+
+% Name of record - Load data
+name_mat = strcat(name,'m.mat');
+name_edf = strcat(name,'.edf');
+
+[struct,val_ECG]=edfread(nameECG_edf);
+%% Load header
+addpath(Folder_header);
+name_info = strcat(name,'m.hea');
+infoName = strcat(name,'m.info');
+m_infoFid = fopen(m_infoName, 'rt');
+fgetl(m_infoFid); fgetl(m_infoFid); %skip 3 line of info file
+fgetl(m_infoFid);
+fgetl(m_infoFid);
+fgetl(m_infoFid); %skip signal header string
+%read signal list into workspace
+for i = 1:size(val_ECG,1)
+    [m_row(i), m_signal(i), m_gain(i), m_base(i), m_units(i)]=strread(fgetl(m_infoFid),'%d%s%f%f%s','delimiter','\t');
+end
+fclose(m_infoFid);
+
+%% Load data ECG & PPG 
+ m_heaFid = fopen(nameECG_info, 'rt');
+    m_header = textscan(fgetl(m_heaFid), '%s %f %f %d %s %s','Delimiter',' ');  %get first line from header
+    for n = 1:size(val_ECG,1)
+        m_signal = textscan(fgetl(m_heaFid), '%s %s %s %d %d %d %d %d %s','Delimiter',' ');
+        n_sigCheck = m_signal{9};
+        n_sigCheck = n_sigCheck{1};
+        switch n_sigCheck
+            case 'II'
+                n_ECG = val_ECG(n,:);
+                n_ECG(n_ECG==-32768) = NaN;
+                n_ECG = (n_ECG - m_base(n)) ./ m_gain(n);
+            case 'PPG'
+                n_PPG = val_ECG(n,:);
+                n_PPG(n_PPG==-32768) = NaN;
+                n_PPG = (n_ECG - m_base(n)) ./ m_gain(n);
+        end
+        %% Check if n_PULSE exist
+        if (exist('n_PPG','var')==1)
+            disp(name);
+        end
+    end
+    fclose('all');
+    
+%% Scale timeline
+n_starttime = struct.startitme;
+m_starttime =
 end
--- load_num.m
+++ load_num.m
@@ -1,4 +1,46 @@
-function [n_SpO2,n_PULSE,n_RESP] = load_num(n_name)
+function [n_SpO2,n_PULSE,n_RESP] = load_num(name)
+%% Setup
 Folder = 'E:\Thesis\Firmware\Data\Mat_num';
 cd(Folder);
+
+% Name of record - Load data
+mat_num = strcat(name,'nm.mat');
+load(name_mat,'val_numeric');
+
+%% Load header
+name_info = strcat(name,'nm.hea');
+m_heaFid = fopen(name_info, 'rt');
+m_header = textscan(fgetl(m_heaFid), '%s %f %f %d %s %s','Delimiter',' ');  %get first line from header
+m_starttime = m_header{6};
+m_starttime = m_starttime{1};
+for n = 1:size(val_numeric,1)
+    m_signal = textscan(fgetl(m_heaFid), '%s %s %s %d %d %d %d %d %s','Delimiter',' ');
+    n_sigCheck = m_signal{9};
+    n_sigCheck = n_sigCheck{1};
+    switch n_sigCheck
+        case 'SpO2'
+            n_SpO2 = val_numeric(n,:);
+            n_SpO2(n_SpO2==-32768) = NaN;
+            n_SpO2 = n_SpO2./10;
+        case 'HR'
+            n_HR = val_numeric(n,:);
+            n_HR(n_HR==-32768) = NaN;
+            n_HR = n_HR./10;
+        case 'PULSE'
+            n_PULSE = val_numeric(n,:);
+            n_PULSE(n_PULSE==-32768) = NaN;
+            n_PULSE = n_PULSE./10;
+        case 'RESP'
+            n_RESP = val_numeric(n,:);
+            n_RESP(n_RESP==-32768) = NaN;
+            n_RESP = n_RESP./10;
+    end
+    %% Check if n_PULSE exist
+    if (exist('n_PULSE','var')==0)
+        n_PULSE = n_HR;
+    end
+end
+fclose('all');
+%% Scale timeline
+n_starttime =
 end
